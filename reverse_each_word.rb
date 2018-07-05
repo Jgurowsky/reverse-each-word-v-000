2378100b9c065f4434da83e345082d1e7bd6c9ee
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-05 08:40:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(words)
   sentence_array = []
-  words.split("").each do |word| 
+  words.split(",").each do |word| 
     sentence_array << word.reverse
   end
   sentence_array.join(" ")
