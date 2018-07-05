e859912c34c1bf81cafd953f16b111952ec3bf98
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-05 09:00:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-def reverse_each_word(words)
-  reversed_words = []
-  words.split.each do |word|
-    reversed_words << word.reverse
-  end
-  reversed_words.join(" ")
-end
+#def reverse_each_word(words)
+  #reversed_words = []
+  #words.split.each do |word|
+    #reversed_words << word.reverse
+  #end
+  #reversed_words.join(" ")
+#end
 
 def reverse_each_word(words)
   words.split.collect do |word|
